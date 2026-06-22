dd37350d4cd1fb467e4d0b73555ecd7496cef3e4
zyTi870 <109009030+zyTi870@users.noreply.github.com>
2026-06-23 02:48:39 +0800

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,15 +1,29 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineNitroConfig } from 'nitropack/config';
+import { fileURLToPath, URL } from 'node:url'
+import { defineNitroConfig } from 'nitropack/config'
 
 export default defineNitroConfig({
   compatibilityDate: '2025-04-24',
+
   srcDir: 'server',
-  alias: { '@': fileURLToPath(new URL('./', import.meta.url)) },
+
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url))
+  },
+
   typescript: {
     generateRuntimeConfigTypes: false,
     generateTsConfig: false
   },
+
   esbuild: {
-    options: { target: 'es2020' }
+    options: {
+      target: 'es2020'
+    }
+  },
+
+  cloudflare: {
+    deployConfig: {
+      name: 'bilibili-real-url'
+    }
   }
-});
+})
